db666e8443bba78171f906f162bc6ed4c27a9638
Michal Goluch <goluch.michal89@gmail.com>
2018-04-04 20:27:34 +0200

    add new string methods for task

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,18 @@
 var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+var dinosaur = "triceratops";
+
+var dinosaurUpperCase = dinosaur.toUpperCase();
+
+console.log(dinosaurUpperCase);
+
+var textReplace = text.replace("Velociraptor" , dinosaurUpperCase);
+
+console.log(textReplace);
+
+var textReplaceLength = (textReplace.length)/2;
+
+console.log(textReplaceLength);
+
+var textReplaceLengthSubString = textReplace.substr(0, textReplaceLength);
+
+console.log(textReplaceLengthSubString);
