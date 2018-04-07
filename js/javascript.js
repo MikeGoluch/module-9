21f3c4efd0e67aaaf45f0bdc7561c7da1f99e6be
Michal Goluch <goluch.michal89@gmail.com>
2018-04-07 09:37:17 +0200

    test script

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,30 @@
-var star = "*";
-var space = " ";
-
-
 function drawTree(numberOfRows) {
-    var star = "";
-    var space = "";
     for(var i = 0 ; i < numberOfRows ; i++) {
+        var level = "";
         for(var j = 0 ; j < numberOfRows - i ; j++) {
-            space = space + " ";
+            level += " ";
         }
         for(var k = 0 ; k <= i * 2 ; k++) {
-            star = star + "*";
+            level += "*";
         }
-        var spaceStar = space + star;
-        console.log(spaceStar);
+        console.log(level);
     }
 }
 
 console.log(drawTree(5));
+
+/*
+function drawTree(numberOfRows) {
+    for(var i = 0 ; i < numberOfRows ; i++) {
+        for(var j = 0 ; j < numberOfRows - i ; j++) {
+            document.write(" ");
+        }
+        for(var k = 0 ; k <= i * 2 ; k++) {
+            document.write("*");
+        }
+        console.log("\n");
+    }
+}
+
+console.log(drawTree(5));
+*/
