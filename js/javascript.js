8958bea25dd0e3db4c0e9ba829655db1265bc487
Michal Goluch <goluch.michal89@gmail.com>
2018-04-08 15:31:35 +0200

    change command lines, test script

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,15 @@
 var buttonClass = document.getElementsByClassName("button");
 console.log(buttonClass);
+
+var newUlElem = document.createElement("ul");
+console.log(newUlElem);
+
 for(var i = 0 ; i < buttonClass.length ; i++) {
-    var newElem = document.createElement("p");
 
-    newElem.innerText = buttonClass[i];
+    var newLiElem = document.createElement("li");
 
-    buttonClass[i].appendChild(newElem);
+    newLiElem = buttonClass[i].innerHTML;
+    console.log(newLiElem);
 
+    newUlElem.appendChild(newLiElem);
 }
