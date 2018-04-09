dc11f748662f1b9186114cffa50c0a3c6d1b3946
Michal Goluch <goluch.michal89@gmail.com>
2018-04-09 19:10:50 +0200

    add minor fix to script, change variable name

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,8 @@
 var list = document.getElementById('list');
-var add = document.getElementById('addElem');
+var addButtonElem = document.getElementById('addElem');
 
-add.addEventListener("click" , function listenerButton() {
+addButtonElem.addEventListener("click" , function listenerButton() {
     var element = document.createElement("li");
-    element.innerHTML = "item";
     var elementNumber = document.getElementsByTagName("li");
     var elementIndex = elementNumber.length;
     element.innerHTML = "item " + elementIndex;
