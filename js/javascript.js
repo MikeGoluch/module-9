d5a4e5d6e674c9bafd970a0108801c903cd7a926
Michal Goluch <goluch.michal89@gmail.com>
2018-04-09 19:02:36 +0200

    add fix to appendChild

--- js/javascript.js
+++ js/javascript.js
@@ -9,7 +9,11 @@ for(var i = 0 ; i < buttonClass.length ; i++) {
     var newLiElem = document.createElement("li");
 
     newLiElem = buttonClass[i].innerHTML;
+    
+    var newLiElemDom = document.createTextNode(newLiElem);
+
     console.log(newLiElem);
 
-    newUlElem.appendChild(newLiElem);
+    newUlElem.appendChild(newLiElemDom);
 }
+
